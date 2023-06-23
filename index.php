386d8b28aadd30abf698706d9d3401125446f43f
Dev Abolfazl <135523419+devkaj@users.noreply.github.com>
2023-06-23 17:01:02 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Hello my friend
+
+// I have managed to write a chat web service with gpt artificial intelligence, which works for free and does not require an open ai account charge.
+
+// I am selling this source for 150 trx
+
+// You can check the link below to test this web service:
+
+// https://gpt.irateam.ir/api/ai.php?type=easy&text=hi and https://gpt.irateam.ir/api/ai.php?text=hi
+
+// To buy, send a message to my Telegram ID: https://t.me/abolfazlkaj
